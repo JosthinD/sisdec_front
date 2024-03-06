429615d820766b3b7e41a8ab76c07c8989dd42f1
DESKTOP-62H9VHB\Josthin <cadetejd15@gmail.com>
2024-03-05 23:40:10 -0500

    Front con las funcionalidades de sesion realizadas.

--- js/login.js
+++ js/login.js
@@ -30,6 +30,8 @@ document.getElementById('login-form').addEventListener('submit', function(event)
   const email = document.getElementById('email').value;
   const password = document.getElementById('password').value;
 
+  sessionStorage.setItem('email', email);
+
   fetch(`${url}?email=${email}&password=${password}`, {
     method: 'GET',
     headers: {
--- js/welcomeadmin.js
+++ js/welcomeadmin.js
@@ -5,56 +5,196 @@ menuButton.addEventListener('click', () => {
     menuOptions.style.display = menuOptions.style.display === 'block' ? 'none' : 'block';
 });
 
+const myModal = document.getElementById('myModal');
+const myModalCloseButton = document.getElementById('myModalClose');
+
+myModalCloseButton.addEventListener('click', () => {
+    myModal.style.display = 'none';
+});
+
 const cerrarSesionButton = document.querySelector('button[data-action="cerrar-sesion"]');
 
 cerrarSesionButton.addEventListener('click', () => {
     window.location.href = 'index.html#';
 });
 
-const editUserButton = document.getElementById('editUserButton');
+const editUserButton = document.querySelector('button:nth-child(3)');
 const modal = document.getElementById('myModal');
 
 editUserButton.addEventListener('click', () => {
-  modal.style.display = 'block';
+    // Obtener el correo electrónico guardado en sessionStorage
+    const email = sessionStorage.getItem('email');
+
+    // Realizar la solicitud FETCH para obtener los datos del usuario
+    fetch(`https://localhost:7215/api/Users/GetAllDataUser?email=${email}`, {
+        method: 'GET',
+        headers: {
+          'accept': '*/*'
+        }
+    })
+    .then((response) => {
+        if (!response.ok) {
+            throw new Error('Error al obtener los datos del usuario');
+        }
+        return response.json();
+    })
+    .then((data) => {
+      // Mostrar los datos del usuario en el modal
+      document.getElementById('id').value = data.data.id;
+      document.getElementById('firstName').value = data.data.primerNombre;
+      document.getElementById('secondName').value = data.data.segundoNombre;
+      document.getElementById('lastName').value = data.data.primerApellido;
+      document.getElementById('secondLastName').value = data.data.segundoApellido;
+      document.getElementById('phone').value = data.data.telefono;
+      document.getElementById('emailget').value = data.data.correo;
+  
+      // Mostrar el modal
+      modal.style.display = 'block';
+    })
+    .catch((error) => {
+        // Manejar el error
+        alert('Error al obtener los datos del usuario');
+        console.error(error);
+    });
+
+});
+
+const enviarButton = document.querySelector('.Users-form button');
+
+enviarButton.addEventListener('click', (event) => {
+    event.preventDefault(); // Evita que el formulario se envíe
+
+    const myHeaders = new Headers();
+    myHeaders.append("accept", "*/*");
+    myHeaders.append("Content-Type", "application/json");
+
+    const raw = JSON.stringify({
+        "idUser": document.getElementById('id').value,
+        "primerNombre": document.getElementById('firstName').value,
+        "segundoNombre": document.getElementById('secondName').value,
+        "primerApellido": document.getElementById('lastName').value,
+        "segundoApellido": document.getElementById('secondLastName').value,
+        "telefono": document.getElementById('phone').value,
+        "correo": document.getElementById('emailget').value
+    });
+
+    const requestOptions = {
+        method: "PUT",
+        headers: myHeaders,
+        body: raw,
+        redirect: "follow"
+    };
+
+    fetch("https://localhost:7215/api/Users/PutDataUser", requestOptions)
+    .then((response) => response.json()) // Convertir la respuesta a JSON
+    .then((result) => {
+        alert(result.message); // Mostrar la respuesta en una alerta
+        console.log(result); // Opcional: Mostrar la respuesta en la consola
+    })
+    .catch((error) => console.error(error));
 });
 
 
+const cambiarContraseñaButton = document.querySelector('button:nth-child(4)');
+const cambiarContraseñaModal = document.getElementById('cambiarContraseñaModal');
+const cambiarContraseñaCloseButton = document.querySelector('#cambiarContraseñaModal .close');
 
-const url = 'https://localhost:7215/api/Users';
+//cambiarContraseñaButton.addEventListener('click', () => {
+//    cambiarContraseñaModal.style.display = 'block';
+//});
 
-document.addEventListener('DOMContentLoaded', function() {
-    const email = sessionStorage.getItem('email');
-  
-    fetch(`${url}?email=${email}`, {
+cambiarContraseñaCloseButton.addEventListener('click', () => {
+    cambiarContraseñaModal.style.display = 'none';
+});
+
+cambiarContraseñaButton.addEventListener('click', () => {
+  // Obtener el correo electrónico guardado en sessionStorage
+  const email = sessionStorage.getItem('email');
+  cambiarContraseñaModal.style.display = 'block';
+
+  // Realizar la solicitud FETCH para obtener los datos del usuario
+  fetch(`https://localhost:7215/api/Users/GetAllDataUser?email=${email}`, {
       method: 'GET',
       headers: {
         'accept': '*/*'
       }
-    })
-    .then((response) => response.json()) // Convertir la respuesta a JSON
-    .then((result) => {
-      if (result.isSuccess) {
-        // Mostrar los datos en los campos del formulario
-        document.getElementById('firstName').value = result.data.prinombre;
-        document.getElementById('secondName').value = result.data.segnombre;
-        document.getElementById('lastName').value = result.data.priapellido;
-        document.getElementById('secondLastName').value = result.data.segapellido;
-        document.getElementById('phone').value = result.data.telefono;
-        document.getElementById('emailget').value = result.data.correo;
-      } else {
-        // Mostrar un mensaje de error en un alert
-        alert(result.message);
+  })
+  .then((response) => {
+      if (!response.ok) {
+          throw new Error('Error al obtener los datos del usuario');
       }
-    })
-    .catch((error) => {
-      // Mostrar un alert con el mensaje de error del catch
-      alert("Lo sentimos, estamos experimentando problemas técnicos. Por favor, inténtalo de nuevo más tarde. Error: 500");
-    });
+      return response.json();
+  })
+  .then((data) => {
+    // Mostrar los datos del usuario en el modal
+    document.getElementById('id_user').value = data.data.id;
+
+    // Mostrar el modal
+    modal.style.display = 'modal_camcont';
+  })
+  .catch((error) => {
+      // Manejar el error
+      alert('Error al obtener los datos del usuario');
+      console.error(error);
   });
 
+});
 
-  const cerrarEdiUsuarioButton = document.querySelector('button[data-action="cerrar-editarusuario"]');
 
-cerrarEdiUsuarioButton.addEventListener('click', () => {
-    modal.style.display = 'none';
+const newPassword = document.getElementById('newPassword').value;
+const confirmNewPassword = document.getElementById('confirmNewPassword').value;
+
+const enviarcontrButton = document.querySelector('#cambiarContraseñaModal button');
+
+enviarcontrButton.addEventListener('click', (event) => {
+    event.preventDefault(); // Evita que el formulario se envíe
+
+    if (newPassword !== confirmNewPassword) {
+        alert("Las contraseñas no coinciden. Por favor, asegúrate de que las contraseñas coincidan.");
+        // Puedes agregar más código aquí para manejar la situación, como limpiar los campos de contraseña.
+    } else {
+        
+        const userId = document.getElementById('id_user').value;
+        const oldPassword = document.getElementById('oldPassword').value;
+
+        fetch(`https://localhost:7215/api/Users/VerifyPasswordForUser?userId=${userId}&contraseña=${oldPassword}`, {
+        method: 'GET',
+        headers: {
+            'Accept': '*/*'
+        }
+        })
+        .then(response => response.json())
+        .then((result) => {
+            
+            console.log(result); // Opcional: Mostrar la respuesta en la consola
+
+            if (result.isSuccess) {
+
+                const userId = document.getElementById('id_user').value;
+                const newPassword = document.getElementById('newPassword').value;
+
+
+                const requestOptions = {
+                method: 'PUT',
+                headers: {
+                    'accept': '*/*'
+                },
+                redirect: 'follow'
+                };
+
+                fetch(`https://localhost:7215/api/Users/UpdateUserPassword?userId=${userId}&contraseña=${newPassword}`, requestOptions)
+                .then(response => response.text())
+                .then(result => console.log(result))
+                .catch(error => console.error(error));
+
+            
+            }
+
+
+
+        });
+
+
+    }
+
 });
